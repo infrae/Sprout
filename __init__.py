a7c1370eab3a0b2f0f4a2514cfdd939479f9cb96
Martijn Faassen <faassen@startifact.com>
2005-05-06 12:01:46 +0000

    Simplify setup.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,11 @@
 import os
 import sys
 
-# a bit hackish, but this allows using the Sprout package as a Zope
-# product, it will add all dirs in the 'src' directory to the path,
-# making them available as Python packages
-srcpath = '%s/src' % (os.sep.join(os.path.split(__file__)[:-1]))
-sys.path.append(srcpath)
-for file in os.listdir(srcpath):
-    filepath = '%s%s%s' % (srcpath, os.sep, file)
-    if os.path.isdir(filepath) and not file == 'CVS':
-        sys.path.append(filepath)
+# this allows the sprout package to be installed as a Zope product.
+# It will add the src directory to the PYTHONPATH
+product_dir, filename = os.path.split(__file__)
+src_path = os.path.join(product_dir, 'src')
+sys.path.append(src_path)
 
 def initialize(context):
     pass
